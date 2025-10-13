d6b52442799625f9a23f28236afd067cb989c38c
Arul Jayaraj <aruljayarajj826@gmail.com>
2025-10-14 01:37:22 +0530

    dfnsdjngfs

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,4 +1,3 @@
-from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
@@ -7,7 +6,7 @@ import os
 load_dotenv()
 DB_URL=str(os.getenv("DB_URL"))
 
-engine = create_engine(DB_URL,echo=False,pool_size=5,max_overflow=10,pool_timeout=30,pool_recycle=1800)
+engine = create_async_engine(DB_URL,echo=False,pool_size=5,max_overflow=10,pool_timeout=30,pool_recycle=1800)
 
 AsyncSessionLocal = sessionmaker(bind=engine,class_=AsyncSession,expire_on_commit=False,autoflush=False,autocommit=False,)
 
