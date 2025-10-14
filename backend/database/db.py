6ace2dccb53fd6c1cfe26a4d392c3f898e800d42
Arul Jayaraj <aruljayarajj826@gmail.com>
2025-10-14 11:56:39 +0530

    dfnsdjngfs

--- backend/database/db.py
+++ backend/database/db.py
@@ -4,20 +4,11 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-DB_URL = str(os.getenv("DB_URL"))
+DB_URL=str(os.getenv("DB_URL"))
 
-engine = create_engine(
-    DB_URL,
-    pool_size=5,        
-    max_overflow=0,         
-    pool_pre_ping=True  
-)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+engine = create_engine(DB_URL)
+SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+session = SessionLocal()
 
 def get_DB():
     db = SessionLocal()
