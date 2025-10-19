5caab9bbed2ae77291c5a7d65284f0d28d3d689d
Arul Jayaraj <aruljayarajj826@gmail.com>
2025-10-19 16:58:56 +0530

    improve

--- backend/models/user_info.py
+++ backend/models/user_info.py
@@ -1,11 +1,10 @@
-from sqlalchemy import Column,ARRAY,String,Text,DateTime,func,BigInteger
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column,ARRAY,String,Text,DateTime,func,BigInteger,ForeignKey
+from sqlalchemy.orm import declarative_base,relationship
+from client_info import Base
 
-Base1 = declarative_base()
-
-class userTable(Base1):
+class userTable(Base):
     __tablename__ = "user_table"
-    client_id = Column(String(30),index=True)
+    client_id = Column(String(30), ForeignKey("Client_table.client_id"),index=True)
     user_id = Column(String(30),primary_key=True,index=True)
     App_id = Column(ARRAY(String),nullable=True)
     Role = Column(String(20),nullable=False)
@@ -17,3 +16,4 @@ class userTable(Base1):
     user_State = Column(String(20),nullable=True)
     user_district = Column(String(20),nullable=True)
     created_by = Column(DateTime,default=func.now()) 
+    client = relationship("ClientTable", backref="users")
